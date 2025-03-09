6f1b2c607f498f2ad57263fd5f2afa120c16f691
Amir Mehr <amir.saiedmehr@gmail.com>
2025-03-08 21:45:36 -0700

    Fix build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css';
+import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@/components/ui/theme-provider';
 import Layout from '@/components/layout/Layout';
